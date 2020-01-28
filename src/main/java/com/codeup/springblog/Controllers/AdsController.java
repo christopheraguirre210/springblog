b4d8e70cc7f8de24c549be62b95289378aad4248
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-28 14:22:38 -0600

    jpa walkthrough follow along

--- src/main/java/com/codeup/springblog/Controllers/AdsController.java
+++ src/main/java/com/codeup/springblog/Controllers/AdsController.java
@@ -1,13 +1,51 @@
 package com.codeup.springblog.Controllers;
 
+import com.codeup.springblog.Models.Ad;
+import com.codeup.springblog.repositories.AdRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class AdsController {
 
+    //Dependency Injection
+    private AdRepository adsDao;
+
+    public AdsController(AdRepository adsDao) {
+        this.adsDao = adsDao;
+    }
+
     @GetMapping("/ads")
     public String adsPage(){
         return "index";
+    }
+
+    @GetMapping("/ads/jpa")
+    @ResponseBody
+    public List<Ad> returnAds(){
+        return adsDao.findAll();
+    }
+
+    @GetMapping("/ads/jpa/create")
+    public void createAd() {
+        Ad ad = new Ad();
+        ad.setTitle("A new ad!");
+        ad.setDescription("This describes the new ad!");
+        adsDao.save(ad);
+    }
+
+    @GetMapping("/ads/order")
+    @ResponseBody
+    public List<Ad> returnAdsByTitle() {
+        return adsDao.findByOrderByTitle();
+    }
+
+    @GetMapping("/ads/search")
+    @ResponseBody
+    public Ad returnAdByTitle() {
+        return adsDao.findByTitle("B");
     }
 }
---
+++ src/main/java/com/codeup/springblog/repositories/AdRepository.java
@@ -0,0 +1,26 @@
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.Models.Ad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AdRepository extends JpaRepository<Ad, Long> {
+
+    List<Ad> findByOrderByTitle();
+
+    // example of derived query
+    Ad findByTitle(String title);
+
+    // examples using JPQL / HQL
+    @Query("from Ad a where a.id like ?1")
+    Ad getAdById(long id);
+
+    @Query("select title from Ad where LENGTH(title) < 10")
+    List<String> getAdsOfCertainTitleLength();
+
+    // using a native query
+    @Query(nativeQuery = true, value = "SELECT title FROM ads WHERE LENGTH(title) < 10")
+    List<String> getAdsOfCertainTitleLengthNative();
+}
